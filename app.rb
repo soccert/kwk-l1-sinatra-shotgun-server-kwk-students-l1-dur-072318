b9e95573ae5614e4f67e0a761bc8af9cb0b32d85
soccert <talyagriffin2004@gmail.com>
2018-07-31 15:20:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to the Talya blog!!!! "
+    "Welcome to the Talya blog!!!!, Get a life "
   end
 
 end
